db1df706aab9f9ef4cfcfec3ff54084503b17b62
Dounhuward B. Caparas <caparasdounhuward@gmail.com>
2024-06-04 17:42:33 +0800

    modify nav button and nav container

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,55 +1,41 @@
-'use client';
+"use client";
 
-import { navlinks } from '@/constants';
-import Link from 'next/link';
-import Image from 'next/image';
-import CustomButton from './CustomButton';
-import { useEffect, useState } from 'react';
-import { motion, useAnimationControls } from 'framer-motion';
+import { navlinks } from "@/constants";
+import Link from "next/link";
+import Image from "next/image";
+import CustomButton from "./CustomButton";
+import { useEffect, useState } from "react";
+import { motion, useAnimationControls } from "framer-motion";
 
 const Navbar = () => {
-  
-    /*Open navigation panel */
+
+  /*Open navigation panel */
   const [open, setOpen] = useState(false);
 
   const handleOpenNav = () => {
-    setOpen(prev => (
-      !prev
-    ))
-  }
-
-  const topVariant = {
-    opened: {
-      rotate: 45,
-    },
-    closed: {
-      rotate: 0,
-    },
+    setOpen((prev) => !prev);
   };
 
-  const midVariant = {
-    opened: {
-      opacity: 0,
-    },
-    closed: {
-      opacity: 1,
-    },
-  };
+  /*End of opening navigation panel */
 
-  const botVariant = {
-    opened: {
-      rotate: -45,
-    },
-    closed: {
-      rotate: 0,
-    },
-  };
+  /*Tap anywhere to close the nav container */
+  useEffect(() => {
+    const handleOutsideClick = () => {
+      if (open) {
+        // If navigation is open and click is outside of navigation, close it
+        setOpen((prev) => !prev);
+      }
+    };
 
-    /*End of opening navigation panel */
+    document.body.addEventListener('click', handleOutsideClick);
+
+    return () => {
+      document.body.removeEventListener('click', handleOutsideClick);
+    };
+  }, [open]);
 
 
-    /*Add background when scrolled */
-
+  /*Add background when scrolled */
   const [scrolled, setScrolled] = useState(false);
 
   useEffect(() => {
@@ -57,42 +43,40 @@ const Navbar = () => {
       window.scrollY > 0 ? setScrolled(true) : setScrolled(false);
     };
 
-    window.addEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
 
     return () => {
-      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener("scroll", handleScroll);
     };
   }, []);
-  
-    /*End of adding background when scrolled */
-  
 
-    /*Animate navigation panel */
-    const containerVariants = {
+  /*End of adding background when scrolled */
+
+  /*Animate navigation panel */
+  const containerVariants = {
     close: {
-      width: '5rem',
+      width: "5rem",
       transition: {
-        type: 'tween',
+        type: "tween",
         duration: 0.3,
       },
     },
     open: {
-      width: '16rem',
+      width: "16rem",
       transition: {
-        type: 'tween',
+        type: "tween",
         duration: 0.3,
       },
     },
   };
 
-
   const containerControls = useAnimationControls();
 
   useEffect(() => {
     if (open) {
-      containerControls.start('open');
+      containerControls.start("open");
     } else {
-      containerControls.start('close');
+      containerControls.start("close");
     }
   }, [open, containerControls]);
 
@@ -101,59 +85,45 @@ const Navbar = () => {
   return (
     <>
       <nav
-        className={`flex justify-between items-center w-full fixed padding 
+        className={`flex max-md:flex-row-reverse justify-between items-center w-full fixed padding 
         ${
           scrolled
-            ? 'bg-slate-900 bg-opacity-50 backdrop-blur-lg border-b border-violet-500 transition ease-in-out duration-300'
-            : ''
+            ? "bg-slate-900 bg-opacity-50 backdrop-blur-lg border-b border-violet-500 transition ease-in-out duration-300"
+            : ""
         }
         `}
       >
-        <Link href='#home' className='font-bold'>
-          Dwr.<span className='text-primary_yellow'>Dev</span>
+        <Link href="#home" className="font-bold">
+          Dwr.<span className="text-primary_yellow">Dev</span>
         </Link>
 
-        <div className='flex justify-center items-center gap-8 max-md:hidden'>
+        <div className="flex justify-center items-center gap-8 max-md:hidden">
           {navlinks.map((link, index) => (
             <div key={index}>
-              <Link href={link.url} className='hover:text-slate-400 uppercase text-sm max-lg:text-[12px]'>
+              <Link
+                href={link.url}
+                className="hover:text-slate-400 uppercase text-sm max-lg:text-[12px]"
+              >
                 {link.title}
               </Link>
             </div>
           ))}
 
           <CustomButton
-            title='Download CV'
-            containerStyles='bg-slate-100 rounded-full px-3 py-1 hover:bg-slate-200 
-                active:bg-slate-100 max-md:hidden'
-            textStyles='text-slate-900 text-[12px]'
+            title="Download CV"
+            containerStyles="bg-slate-100 rounded-full px-3 py-1 hover:bg-slate-200 
+                active:bg-slate-100 max-md:hidden"
+            textStyles="text-slate-900 text-[12px]"
           />
-
         </div>
 
-        
+        <div className="hidden max-md:flex">
+          <button className="flex flex-col gap-y-1" onClick={handleOpenNav}>
 
-        <div className='hidden max-md:flex'>
-          <button
-            className='flex flex-col gap-y-1' onClick={handleOpenNav}
-          >
-            <motion.div
-              variants={topVariant}
-              animate={open ? 'opened' : 'closed'}
-              className={`w-6 h-1 bg-white rounded-md origin-left`}
-            ></motion.div>
+            <div className='w-6 h-1 bg-white rounded-md'></div>
+            <div className='w-6 h-1 bg-white rounded-md'></div>
+            <div className='w-6 h-1 bg-white rounded-md'></div>
 
-            <motion.div
-              variants={midVariant}
-              animate={open ? 'opened' : 'closed'}
-              className={`w-6 h-1 bg-white rounded-md`}
-            ></motion.div>
-
-            <motion.div
-              variants={botVariant}
-              animate={open ? 'opened' : 'closed'}
-              className={`'w-6 h-1 bg-white rounded-md origin-left`}
-            ></motion.div>
           </button>
         </div>
       </nav>
@@ -162,32 +132,30 @@ const Navbar = () => {
         <motion.div
           variants={containerVariants}
           animate={containerControls}
-          initial='close'
-          className='w-2/3 h-full bg-slate-100 padding mt-16 fixed right-0 items-start md:hidden'>  
+          initial="close"
+          className="w-2/3 h-full bg-slate-100 padding fixed left-0 items-start md:hidden"
+        >
+          <div className="mt-4 flex flex-col gap-4 ">
+            {navlinks.map((link, index) => (
+              <div key={index} className="flex justify-start item gap-2">
+                <Image
+                  src={link.icon}
+                  alt={link.title}
+                  height={20}
+                  width={20}
+                  className="object-contain"
+                />
 
-              <div className='mt-4 flex flex-col gap-4 '>
-              {navlinks.map((link, index) => (
-                      <div key={index} className='flex justify-start item gap-2'>
-
-                        <Image 
-                          src={link.icon} 
-                          alt={link.title}
-                          height={20}
-                          width={20}
-                          className='object-contain'
-                        />
-
-                        <Link 
-                          href={link.url} 
-                          onClick={handleOpenNav}
-                          className='text-black'>
-                            {link.title}
-                        </Link>
-                      </div>
-                    ))}
+                <Link
+                  href={link.url}
+                  onClick={handleOpenNav}
+                  className="text-black"
+                >
+                  {link.title}
+                </Link>
               </div>
-                
-                  
+            ))}
+          </div>
         </motion.div>
       )}
     </>
